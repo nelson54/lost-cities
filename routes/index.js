f553b0bc426db6f59543b2960a548b6878482d4a
Shafeen M <sodl02@gmail.com>
2016-12-02 12:53:34 -0500

    authenticate endpoints working

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// TODO: protected profile page / dashboard
+router.get('/profile', isLoggedIn, function (req, res) {
+  res.render('profile', {name: "TestPlayer"});
+});
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+  // if user is authenticated in the session, carry on
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  // redirect them to the home page otherwise
+  res.redirect('/');
+}
+
 module.exports = router;
