4540bd03eb5df36bf558ed9efbd31ca82875479d
Shafeen M <sodl02@gmail.com>
2016-12-03 21:06:50 -0500

    smaller hand cards, improved board with play & discard areas

--- public/dashboard/GameCtrl.js
+++ public/dashboard/GameCtrl.js
@@ -2,10 +2,36 @@ angular.module('lost-cities-game')
 .controller('GameCtrl', function ($http, $window) {
     var game = this;
 
-    game.mainMsg = "All my games";
-    game.subMsg = "Dashboard sub-view";
     game.userId = $window.document.getElementById('userId').innerText;
     game.gameId = $window.document.getElementById('gameId').innerText;
+
+    game.colorStacks = [
+        {
+            color: 'Y',
+            class: 'panel-warning',
+            discardPile: []
+        },
+        {
+            color: 'B',
+            class: 'panel-info',
+            discardPile: []
+        },
+        {
+            color: 'W',
+            class: 'panel-default',
+            discardPile: []
+        },
+        {
+            color: 'G',
+            class: 'panel-success',
+            discardPile: []
+        },
+        {
+            color: 'R',
+            class: 'panel-danger',
+            discardPile: []
+        }
+    ];
 
     // TODO: flesh this out some more
     let GET_GAME_STATE_API = "/api/games/"+game.gameId;
@@ -16,10 +42,17 @@ angular.module('lost-cities-game')
         gameData = res.data;
 
         game.hand = gameData.players.filter((player) => player.id == game.userId)[0].hand;
-        console.log(game.hand);
+        console.log("Current hand: %O", game.hand);
 
+        game.colorStacks.forEach(
+            (stack) => stack.discardPile = gameData.players[0].playArea.playStacksByColor[stack.color]
+        );
     }, function failure() {
         console.log("XHR failed!");
     });
 
+    game.clicked = function (card) {
+        console.log(card);
+        alert('you clicked card '+ card.color+card.number);
+    };
 });
