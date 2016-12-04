952d739ec1285369df65bd3a80027e7a9924775e
Shafeen M <sodl02@gmail.com>
2016-12-04 17:33:44 -0500

    adding initial drag/drop functionality to game view

--- public/dashboard/GameCtrl.js
+++ public/dashboard/GameCtrl.js
@@ -1,5 +1,5 @@
 angular.module('lost-cities-game')
-.controller('GameCtrl', function ($http, $window) {
+.controller('GameCtrl', function ($scope, $http, $window) {
     var game = this;
 
     game.userId = $window.document.getElementById('userId').innerText;
@@ -50,6 +50,50 @@ angular.module('lost-cities-game')
     }, function failure() {
         console.log("XHR failed!");
     });
+
+    game.initDragDrop = function () {
+        $(".face.front").each(function () {
+            $(this).draggable({
+                helper: 'clone',
+                start: function(e, ui) {
+                    $(this).hide();
+                },
+                stop: function(e, ui) {
+                    $(this).show();
+                }
+            });
+        });
+
+        $(".discard-pile").each(function () {
+            $(this).droppable({
+                drop: function (e, ui) {
+                    console.log(e.target);
+                    let container = $(this);
+                    console.log(container);
+                    container.html('dropped card');
+
+                    let draggable = $(ui.draggable);
+                    let card = {
+                        color : draggable.attr('data-color'),
+                        number : draggable.attr('data-number')
+                    };
+                    console.log("card dropped: %O", card);
+                    // TODO: add  the card on top if allowed --> use angular here
+                    container.empty().text("discarded "+card.color+" "+card.number);
+
+                    // remove the card from the hand
+                    // TODO: watch out for multipliers
+                    let cardToRemove = game.hand.filter(
+                        (c) => (c.color == card.color && c.number == (+card.number))
+                    )[0];
+                    game.hand = game.hand.filter(
+                        (c) => (c!=cardToRemove)
+                    );
+                    $scope.$apply();
+                }
+            });
+        });
+    };
 
     game.clicked = function (card) {
         console.log(card);
--- public/js/card-stack/card-stack.js
+++ public/js/card-stack/card-stack.js
@@ -15,17 +15,5 @@ $(document).ready(function () {
             $(this).removeClass("before prefix_" + (i+1));
         });
     });
-    /* add the "flip" function */
-    $(".fan li").click(function(){
-        $(this).find(".card").addClass("flipped").mouseleave(function(){
-            $(this).removeClass("flipped");
-        });
-        return false;
-    });
-    /* set the card's data + bgcolor */
-    $(".face.front").each(function(){
-        $(this).css("background",$(this).data("bgcolor"));
-        $(this).html($(this).data("bgcolor"));
-    });
     $("a[rel='external']").attr("target", "_blank");
 });
